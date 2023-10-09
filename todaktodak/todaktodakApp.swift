a755b5ed147ec39cfa5f9a1ec2683915ef0c9f2b
gwon11225 <gwon11225@gmail.com>
2023-10-09 16:19:38 +0900

    Initial Commit

---
+++ todaktodak/todaktodakApp.swift
@@ -0,0 +1,17 @@
+//
+//  todaktodakApp.swift
+//  todaktodak
+//
+//  Created by Jiyoung on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct todaktodakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
